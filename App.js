c84c81d3ea8a0a1ea89520388097e6aaed405dbb
Krishna <cu.17bcs1796@gmail.com>
2023-11-18 13:13:35 +0530

    :fire:

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View >
+      <Text style={{fontSize:30}}>hello krishna this is my first App</Text>
+      <Button title='Press here'></Button>
+      <TextInput
+      style={{
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+      }}
+       defaultValue='type here' />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
